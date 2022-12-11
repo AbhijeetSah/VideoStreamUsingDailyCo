ba120fe10bff9b9f7635970a95272c5293582ac5
AbhijeetSah <abhijeet.sah5895@gmail.com>
2022-12-11 16:43:05 +0545

    Add project files.

---
+++ DemoVideoStream/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using DemoVideoStream.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DemoVideoStream.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DemoVideoStream/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DemoVideoStream.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
